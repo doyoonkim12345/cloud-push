d65a92398991e6534b96dd6234f91a47727d4bfa
doyoonkim12345 <kdyem0628@gmail.com>
2025-04-26 14:46:28 +0900

    Remove apps/server to convert to submodule

--- apps/server/features/api/browserClient.ts
+++
@@ -1,8 +0,0 @@
-"use server";
-
-import { generateBrowserClient } from "@cloud-push/core/utils";
-import { dbNodeClient, storageNodeClient } from "./nodeClient";
-
-export const storageBrowserClient = generateBrowserClient(storageNodeClient);
-
-export const dbBrowserClient = generateBrowserClient(dbNodeClient);
--- apps/server/features/hash/lib/createHash.ts
+++
@@ -1,9 +0,0 @@
-import crypto, { BinaryToTextEncoding } from "crypto";
-
-export async function createHash(
-  buffer: Buffer,
-  hashingAlgorithm: string,
-  encoding: BinaryToTextEncoding
-) {
-  return crypto.createHash(hashingAlgorithm).update(buffer).digest(encoding);
-}
